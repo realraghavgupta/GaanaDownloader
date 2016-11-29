82412112a769b576cdde6b7e4ec62e20fb5a93f8
pathrik <pathrikumark@gmail.com>
2016-11-29 10:14:40 +0530

    Search Udates - InProgress

--- src/main/java/in/pathri/gaana/downloader/GaanaSearch.java
+++ src/main/java/in/pathri/gaana/downloader/GaanaSearch.java
@@ -20,34 +20,65 @@ public class GaanaSearch {
 	private static JSONArray searchResultsArray = new JSONArray();
 	
 	public static void doSearch(SearchType searchType, Language language) {
+		logger.entry(searchType,language);
 		int totalResult = 0;
-		int pendingItems = 0;
 		int start = 0;
 		int count = 99;
+		int searchCount = 0;
+		JSONObject userData;
+		JSONArray tempSearchResults;
+		
 		Map<String, String> params = getSearchParams(searchType, language);
 		if (params.isEmpty()) {
+			logger.debug("getSearchParams resulted empty params");
 			return;
 		}
-		do{
+		
+		params = updateLimit(start, count);
+		logger.debug("updateLimit resulted::{}",params);
+		userData = sendSearchReq(params);
+		if(null == userData){
+			logger.debug("sendSearchReq resulted empty userData");
+			return;
+		}
+		
+		totalResult = (int) userData.get("count");
+		logger.debug("totalResult of Initial Request::{}",totalResult);
+		searchResultsArray.ensureCapacity(totalResult + 10);
+		
+		tempSearchResults = (JSONArray) userData.get("album");
+		logger.debug("tempSearchResult::{}",tempSearchResults);
+		logger.debug("tempSearchResultSize::{}",tempSearchResults.size());
+		searchResultsArray.addAll(tempSearchResults);
+		logger.debug("searchResultsArray Size:{}",searchResultsArray.size());
+		searchCount = tempSearchResults.size();
+		start = start + searchCount;
+		logger.debug("Start count:{}",start);
+		while(start > totalResult){
 			params = updateLimit(start, count);
-			try {
-				String reponse = HTTPHelper.sendGet(Global.GAANA_BASE_URL, params);
-				Object jsonVal = JSONValue.parse(reponse);			
-				JSONObject userData = (JSONObject)jsonVal;
-				//TODO: Update local data store with data; searchResultsArray
-				//TODO: Get total count and update totalResult
-				//TODO: loop for total count by using limit update
-				//TODO: Update pendingItems & start
-			} catch (Exception e) {
-				logger.catching(e);
-			}			
-		}while(pendingItems > 0);
+			userData = sendSearchReq(params);
+			if(null == userData){
+				return;
+			}
+			tempSearchResults = (JSONArray) userData.get("album");
+			searchResultsArray.addAll(tempSearchResults);
+			searchCount = tempSearchResults.size();
+			start = start + searchCount;			
+		}
+		logger.debug("Search Result::{}",searchResultsArray.toJSONString());
+		//TODO: Update local data store with data; searchResultsArray
+		//TODO: Get total count and update totalResult
+		//TODO: loop for total count by using limit update
+		//TODO: Update pendingItems & start			
+		logger.traceExit();
 
 	}
 
-	private static Map<String, String> getSearchParams(SearchType searchType, Language language) {		
+	private static Map<String, String> getSearchParams(SearchType searchType, Language language) {
+		logger.entry(searchType,language);
 		switch (searchType) {
 		case ALL_ALBUMS:
+			logger.trace("Into ALL_ALBUMS");
 			paramObj.initParams("album", "all_albums");
 			paramObj.setLanguage(language);
 			break;
@@ -55,12 +86,25 @@ public class GaanaSearch {
 		default:
 			break;
 		}		
+		logger.traceExit("Params:{}",paramObj.getParams());
 		return paramObj.getParams();
 	}
 	
 	private static Map<String, String> updateLimit(int start, int count){
 		paramObj.updateLimit(start, count);
 		return paramObj.getParams();
+	}
+	
+	private static JSONObject sendSearchReq(Map<String, String> params){
+		try {
+			String reponse = HTTPHelper.sendGet(Global.GAANA_BASE_URL, params);
+			Object jsonVal = JSONValue.parse(reponse);			
+			JSONObject userData = (JSONObject)jsonVal;
+			return userData;
+		} catch (Exception e) {
+			logger.catching(e);
+		}					
+		return null;
 	}
 
 }
